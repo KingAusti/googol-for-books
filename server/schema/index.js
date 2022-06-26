7ebe85da0bcd02a9db645bbb92b3e56756af0eb7
KingAusti <austin@thehenrys.xyz>
2022-06-26 14:43:47 -0700

    setting up server and initial file structure

---
+++ server/schema/index.js
@@ -0,0 +1,1 @@
+//import and then expors resolvers and typeDefs
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs, resolvers, context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +22,12 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startServer = async (typeDefs, resolvers) => {
+  await server.start()
+  server.applyMiddleware({ app })
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT} GraphQL at http://localhost:${PORT}${server.graphqlPath}`));
+  })
+};
+
+startServer(typeDefs, resolvers);
